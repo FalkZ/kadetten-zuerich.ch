2f600ef639231db7ff8279d2e62527cad3ad944b
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-11-19 19:20:42 +0100

    fixed home

--- src/loader.js
+++ src/loader.js
@@ -1,19 +1,19 @@
-import { app } from "hyperapp";
+import { app } from 'hyperapp';
 
-import "./redirect";
-import "./style.styl";
+import './redirect';
+import './style.styl';
 
-import state from "./state";
-import view from "./view";
-import actions from "./actions";
+import state from './state';
+import view from './view';
+import actions from './actions';
 
 const main = app(state, actions, view, document.body);
 
-console.log(main);
+console.log(window.location.hash);
 
 if (!window.location.hash) {
-  main.redirect("");
+  main.redirect('');
 }
 
 window.onhashchange = () =>
-  main.redirect(window.location.hash.replace("#", ""));
+  main.redirect(window.location.hash.replace('#', ''));
